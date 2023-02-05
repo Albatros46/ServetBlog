47538dfc643c91532adbbcb7130a59c46df86de5
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-02-05 22:21:43 +0100

    Login theme added

--- FineBlog/Program.cs
+++ FineBlog/Program.cs
@@ -1,9 +1,25 @@
+using FineBlog.Data;
+using FineBlog.Models;
+using FineBlog.Utilities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllersWithViews();
+//Veritabani baglantisi
+var connectionStr = builder.Configuration.GetConnectionString("DefaultConnection");
+builder.Services.AddDbContext<ApplicationDbContext>(options=>options.UseSqlServer(connectionStr));
+
+//Identity islemleri
+builder.Services.AddIdentity<ApplicationUser,IdentityRole>().AddEntityFrameworkStores<ApplicationDbContext>().AddDefaultTokenProviders();
+
+//IDbInitializer tanimi
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();//daha sonra dataseeding metodu asagiya tanimlancak
 
 var app = builder.Build();
+Dataseeding();//metod yazildiktan sonra burada calistirilacak
 
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
@@ -20,8 +36,22 @@ app.UseRouting();
 
 app.UseAuthorization();
 
-app.MapControllerRoute(
+app.MapControllerRoute(//admin paneli icin
+    name: "area",
+    //pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+
+app.MapControllerRoute(//normal sayfa icin
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
-
+//pattern: "{area:exists}/{controller=Home}/{action=Index}/{id?}");
+https://www.youtube.com/watch?v=7MolpkyVgZs&list=PLIyNJGgXfvmp92nzScIN0ZIPA_C6O72OP&index=7
 app.Run();
+void Dataseeding()
+{
+    using(var scope = app.Services.CreateScope())
+    {
+        var DbInitializer= scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+        DbInitializer.Initialize();
+    }
+}
---
+++ FineBlog/Utilities/DbInitializer.cs
@@ -0,0 +1,64 @@
+﻿using FineBlog.Data;
+using FineBlog.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+
+namespace FineBlog.Utilities
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly ApplicationDbContext _context;
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public DbInitializer(ApplicationDbContext context, UserManager<ApplicationUser> userManager, RoleManager<IdentityRole> roleManager)
+        {
+            _context = context;
+            _userManager = userManager;
+            _roleManager = roleManager;
+        }
+
+        public void Initialize()
+        {
+            if (!_roleManager.RoleExistsAsync(WebsiteRoles.WebsiteAdmin).GetAwaiter().GetResult())
+            {
+                _roleManager.CreateAsync(new IdentityRole(WebsiteRoles.WebsiteAdmin)).GetAwaiter().GetResult();
+                _roleManager.CreateAsync(new IdentityRole(WebsiteRoles.WebsiteAuthor)).GetAwaiter().GetResult();
+                _userManager.CreateAsync(new ApplicationUser()
+                {
+                    UserName = "admi@gmail.com",
+                    Email = "admi@gmail.com",
+                    FirstName = "Servet",
+                    LastName = "Akcadag",
+                }, "admin@12345").Wait();//password=admin@12345
+                var appUser = _context.ApplicationUsers.FirstOrDefault(x => x.Email == "admi@gmail.com");
+                if (appUser != null)
+                {
+                    _userManager.AddToRoleAsync(appUser, WebsiteRoles.WebsiteAdmin).GetAwaiter().GetResult();
+                }
+               
+                var listOfPages = new List<Page>()
+                {
+                   new Page()
+                   {
+                        Title="Über Uns",
+                        Slug= "über uns"
+                   },
+                    new Page()
+                    {
+                        Title = "Kontakt",
+                        Slug = "kontakt"
+                    },
+                    new Page()
+                    {
+                        Title = "Privacy Policy",
+                        Slug = "privacy"
+                    }
+                };
+                _context.Pages.AddRange(listOfPages);
+                _context.SaveChanges();
+
+            }
+        }
+    }
+}
