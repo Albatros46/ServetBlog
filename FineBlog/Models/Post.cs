bdc614258f1100540806c03746a4545200319bd8
SERVET AKÇADAĞ <34627199+Albatros46@users.noreply.github.com>
2023-02-05 16:51:05 +0100

    Add project files.

---
+++ FineBlog/Models/Post.cs
@@ -0,0 +1,15 @@
+﻿namespace FineBlog.Models
+{
+    public class Post
+    {
+        public int Id { get; set; }
+        public string? Title { get; set; }
+        public string? ShortDescription { get; set; }
+        public DateTime CreateDate { get; set; } = DateTime.Now;
+        //Table relation One To One
+        public string? AapplicationUserId { get; set; }
+        public ApplicationUser ApplicationUser { get; set; }
+        public string? Description { get; set; }
+        public string? Slug { get; set; }
+    }
+}
